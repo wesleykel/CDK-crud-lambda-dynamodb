3d2123408aff583fb1d9afa110d08747a3eeef6d
wesleykel <wesley.kelly@hotmail.com>
2024-03-01 14:09:32 +0000

    Initial commit

---
+++ test/cdk-crud-lambda-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCrudLambdaDynamodb from '../lib/cdk-crud-lambda-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-crud-lambda-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCrudLambdaDynamodb.CdkCrudLambdaDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
